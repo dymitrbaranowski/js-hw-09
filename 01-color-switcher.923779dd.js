e710a743b6cb08c3220406040387f45d3a206107
dymitrbaranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-10-31 13:34:52 +0000

    Deploying to gh-pages from @ dymitrbaranowski/js-hw-09.1@92751f5b8e5de5f7c4ca202302e801e965634f9a 🚀

---
+++ 01-color-switcher.923779dd.js
@@ -0,0 +1,2 @@
+!function(){var t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]")};t.btnStop.disabled=!0;var n=null;t.btnStart.addEventListener("click",(function(){t.btnStart.disabled=!0,t.btnStop.disabled=!1,n=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),t.btnStop.addEventListener("click",(function(){clearInterval(n),t.btnStart.disabled=!1,t.btnStop.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.923779dd.js.map
